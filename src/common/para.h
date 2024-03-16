3889372e265040d4765ad63ea0104764dadf4dea
beacon lee <bquanlicn@gmail.com>
2024-03-16 23:03:29 +0800

    feat: add para
    添加了 para，在进行 hybrid a star 规划时进行使用的

---
+++ src/common/para.h
@@ -0,0 +1,47 @@
+#pragma once
+
+namespace beacon
+{
+struct Para
+{
+  int minx_;
+  int miny_;
+  int minyaw_;
+  int maxx_;
+  int maxy_;
+  int maxyaw_;
+  int xw_;
+  int yw_;
+  int yaww_;
+  double xyreso_{};
+  double yawreso_;
+
+  Para(int minx,
+       int miny,
+       int minyaw,
+       int maxx,
+       int maxy,
+       int maxyaw,
+       int xw,
+       int yw,
+       int yaww,
+       double xyreso,
+       double yawreso)
+    : minx_(minx)
+    , miny_(miny)
+    , minyaw_(minyaw)
+    , maxx_(maxx)
+    , maxy_(maxy)
+    , maxyaw_(maxyaw)
+    , xw_(xw)
+    , yw_(yw)
+    , yaww_(yaww)
+    , xyreso_(xyreso)
+    , yawreso_(yawreso)
+  {}
+
+  Para(const Para&) = default;
+
+  ~Para() = default;
+};
+} // namespace beacon

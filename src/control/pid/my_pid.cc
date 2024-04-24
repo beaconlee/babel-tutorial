445d673b40ce878bcc86f6e71862cb657936c32d
yydylg <bquanabc@gmail.com>
2024-04-24 11:26:25 +0800

    feat: 大致完成自己关于 pid 的一个实现，从中发现了很多问题
    问题 1. 没有设置对是否会进入 init
        2. 积分项累积过大，变化特别明显
        3. 波动非常大，没有向目标靠拢
        4. 直接将 pid 输出的值作为最终的结果，没有将其附加到控制变量上
    
        5. 当出现 控制值变得异常大时，可能时 pid 中的 d 过大了
        6. 当需要较长时间才能进入到一个稳定状态时：可能是 i 相关
    
     犯了一个非常重大的错误,pid 的 error 的计算是 target - real

--- src/control/pid/my_pid.cc
+++ src/control/pid/my_pid.cc
@@ -8,113 +8,128 @@
 #include <algorithm>
 
 
-class PidConf
-{
-public:
-  PidConf(double kp, double ki, double kd, double i_limit = 50)
-    : kp_(kp)
-    , ki_(ki)
-    , kd_(kd)
-    , i_limit_(i_limit)
-  {}
-
-  PidConf(const PidConf& pidconf)
-    : PidConf(pidconf.kp_, pidconf.ki_, pidconf.kd_, pidconf.i_limit_)
-  {}
-
-  void SetKp(double kp)
-  {
-    kp_ = kp;
-  }
-  void SetKi(double ki)
-  {
-    ki_ = ki;
-  }
-  void SetKd(double kd)
-  {
-    kd_ = kd;
-  }
-
-  double GetKp() const
-  {
-    return kp_;
-  }
-  double GetKi() const
-  {
-    return ki_;
-  }
-  double GetKd() const
-  {
-    return kd_;
-  }
-
-private:
-  double kp_;
-  double ki_;
-  double kd_;
-
-  double i_limit_;
-};
-
-
 class PidControl
 {
 public:
-  PidControl(const PidConf& pid_conf): pid_conf_(pid_conf) {}
+  PidControl() = default;
 
-  double Control(double error)
+  void Init()
   {
-    ki_all_ += error;
-    ki_all_ = std::min(100., ki_all_);
-    return -pid_conf_.GetKp() * error -pid_conf_.GetKi() * ki_all_ +
-           pid_conf_.GetKd() * error;
+    integrator_enabled_ = true;
   }
 
-  void SetKp(double kp)
+  double Control(double error, double dt)
   {
-    pid_conf_.SetKp(kp);
-  }
-  void SetKi(double ki)
-  {
-    pid_conf_.SetKi(ki);
-  }
-  void SetKd(double kd)
-  {
-    pid_conf_.SetKd(kd);
+    double diff{0.};
+    if(!first_hit_)
+    {
+      // diff = (previous_error_ - error) / dt;
+      diff = (error - previous_error_) / dt;
+    }
+    else
+    {
+      first_hit_ = false;
+    }
+    previous_error_ = error;
+
+    if(integrator_enabled_)
+    {
+      if(integrator_holder_)
+      {
+        integrator_ = 0;
+      }
+      else
+      {
+        integrator_ += ki_ * error;
+        if(integrator_ > integrator_saturation_high_)
+        {
+          integrator_ = integrator_saturation_high_;
+          integrator_saturation_status_ = 1;
+        }
+        else if(integrator_ < integrator_saturation_low_)
+        {
+          integrator_ = integrator_saturation_low_;
+          integrator_saturation_status_ = -1;
+        }
+        else
+        {
+          integrator_saturation_status_ = 0;
+        }
+        std::cout << "integrator: " << integrator_ << "  ";
+      }
+    }
+
+    std::cout << "error : " << error << "   diff: " << diff << '\n';
+    // output_ = error * kp_ - integrator_ + diff * kd_;
+    output_ = error * kp_ + integrator_ + diff * kd_;
+    std::cout << "output: " << output_ << "\n";
+    return output_;
   }
 
-  double GetKp() const
+  void SetPID(double kp, double ki, double kd)
   {
-    return pid_conf_.GetKp();
-  }
-  double GetKi() const
-  {
-    return pid_conf_.GetKi();
-  }
-  double GetKd() const
-  {
-    return pid_conf_.GetKd();
+    kp_ = kp;
+    ki_ = ki;
+    kd_ = kd;
+
+    integrator_saturation_high_ = 100;
+    integrator_saturation_low_ = -100;
   }
 
 private:
-  PidConf pid_conf_;
-  double ki_all_{0};
+  double kp_{0.};
+  double ki_{0.};
+  double kd_{0.};
+  double yaw_{0.};
+
+  double integrator_{0.};
+
+  double integrator_saturation_high_{std::numeric_limits<double>::max()};
+  double integrator_saturation_low_{std::numeric_limits<double>::min()};
+  // 积分饱和状态 之前没有添加
+  double integrator_saturation_status_{0};
+
+  bool first_hit_{true};
+
+  bool integrator_enabled_{true};
+  bool integrator_holder_{false};
+
+  double previous_error_{0.};
+  double previous_otput_{0.};
+
+
+  double output_{0.};
 };
 
 
 int main()
 {
-  std::cout << "当前水温：" << 0 << "  目标水温：" << 100;
+  std::cout << "当前水温：" << 0 << "  目标水温：" << 100 << "\n";
   double temple = 0.;
 
-  PidConf pidconf(0.5, 0.5, 0.5);
-  PidControl pid(pidconf);
+  PidControl pid;
+  pid.Init();
+  pid.SetPID(0.6, 0.4, 0.5);
   while(true)
   {
     // std::this_thread::sleep_for(std::chrono::duration<std::chrono::seconds, std::ratio<1, 1>>(1));
     std::this_thread::sleep_for(std::chrono::seconds(1));
     std::cout << "当前的 temple: " << temple << "\n";
-    temple = pid.Control(temple - 100);
+    // temple = pid.Control(temple - 100, 1);
+    // 犯了一个非常重大的错误，pid 的 error 的计算是 target - real
+    temple += pid.Control(100 - temple, 1);
   }
   return 0;
 }
+
+// 问题 1. 没有设置对是否会进入 init
+//     2. 积分项累积过大，变化特别明显
+//     3. 波动非常大，没有向目标靠拢
+//     4. 直接将 pid 输出的值作为最终的结果，没有将其附加到控制变量上
+
+//     5. 当出现 控制值变得异常大时，可能时 pid 中的 d 过大了
+//     6. 当需要较长时间才能进入到一个稳定状态时：可能是 i 相关
+//
+//
+//
+//    💌 犯了一个非常重大的错误，pid 的 error 的计算是 target - real
